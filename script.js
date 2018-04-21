c21464923c60fc2201c7f3cde637fb62aa1b87a1
Student <student@Admins-MacBook-Air-5.local>
2018-04-21 13:03:38 -0700

    speach text

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-/* remove this comment and place your JavaScript here */ 
+let fight;
+let son;
+let daughter;
+let pageTitle=document.querySelector("h1");
+let pageTitleText=pageTitle.innerHTMl;
+let tryAgain=document.getElementById("try-again");
+let quizWrapper=document.getElementById("quiz-wrapper");
+let result= document.getElementById("result");
+let formSubmit=document.getElementById("form-submit");
+tryAgain.addEventListener("click",resetQuiz);
+formSubmit.addEventListener("click",processResults);
+function processResults(){
+
+}
+function getPersonality(){
+let score=0;
+if(fight.id === "fight") {
+   score += 1;
+ }
+ else if(son.id === "green-peppers") {
+   score += 2;
+ }
+ else if (daughter.id === "green-peppers") {
+
+ }
+
+}
+function resetQuiz(){
+
+}
